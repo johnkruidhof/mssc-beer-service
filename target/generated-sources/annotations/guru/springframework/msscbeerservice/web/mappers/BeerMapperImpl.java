39c868b4048624bd41a8e4fc95073ea8526799ad
johnkruidhof <johnkruidhof@gmail.com>
2020-09-18 16:59:30 +0200

    T/m Mapstruct (85)

---
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbeerservice.web.mappers;
+
+import javax.annotation.processing.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-09-18T12:10:49+0200",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 14.0.1 (N/A)"
+)
+@Component
+@Primary
+public class BeerMapperImpl extends BeerMapperDecorator implements BeerMapper {
+}
