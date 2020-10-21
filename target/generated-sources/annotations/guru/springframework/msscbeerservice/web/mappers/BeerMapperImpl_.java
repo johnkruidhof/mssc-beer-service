2929e0df28fcc4cd7f607901e8d2b4f7871f4cc3
johnkruidhof <johnkruidhof@gmail.com>
2020-10-21 17:11:56 +0200

    Dockerfile added

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-10-13T17:00:49+0200",
+    date = "2020-10-21T17:01:48+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 14.0.1 (N/A)"
 )
 @Component
